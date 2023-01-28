d810eabf342297f82fb1e010aaebd86dc9252026
Trent <tdyck@protonmail.com>
2023-01-28 14:35:19 -0800

    Some bug fixes, and checks on v1.0.1 installation prior to updating

--- update-horizon.sh
+++ update-horizon.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
 
 init(){
+
+	export config_json=$(sudo find /home/deck/.local/share/Steam/steamapps/compatdata/ -name config.json -type f | grep HorizonXI)
+	export config_prefix=$(echo $config_json | sed 's/config.json$//g')
+	export storage_json=$(echo ${config_prefix}storage.json)
+	export base_downloaded_boolean=$(cat $storage_json | jq '.GAME_UPDATER.baseGame.downloaded')
+	export base_extracted_boolean=$(cat $storage_json | jq '.GAME_UPDATER.baseGame.extracted')
+	export updater_downloaded_boolean=$(cat $storage_json | jq '.GAME_UPDATER.updater.downloaded')
+	export updater_extracted_boolean=$(cat $storage_json | jq '.GAME_UPDATER.updater.extracted')
 	export horizon_json=$(curl https://api.github.com/repos/HorizonFFXI/HorizonXI-Launcher-Binaries/releases | jq '.')
-	export latest_version=$(echo ${horizon_json} | jq -r '.[].name' | head -n1)
+	if [[ ${base_downloaded_boolean} == "true" && ${base_extracted_boolean} == "true" && ${updater_downloaded_boolean} == "true" && ${updater_extracted_boolean} == "true" ]]; then
+		export latest_version=$(echo ${horizon_json} | jq -r '.[].name' | head -n1)
+	else
+		# Let's hard code latest version to v1.0.1, since the installer isn't complete we need to download & complete the install on v1.0.1 before updating
+		# See Note: https://github.com/hilts-vaughan/hilts-vaughan.github.io/blob/master/_posts/2022-12-16-installing-horizon-xi-linux.md#install-horizonxi---steam-play-steam-deck--other-systems
+		export latest_version="v1.0.1"
+	fi
 	export download_url=$(echo ${horizon_json} | jq -r '.[] | select(.tag_name=="'${latest_version}'") | .assets[] | select ( .name | endswith ("exe") ) | .browser_download_url')
 	export nupkg_name=$(echo ${horizon_json} | jq -r '.[] | select(.tag_name=="'${latest_version}'") | .assets[] | select ( .name | endswith ("nupkg") ) | .name ')
+	export current_version=$(cat $config_json | jq -r '.__internal__.migrations.version' | sed 's/^/v/g')
+	echo "storage_json: $storage_json"
+	echo "latest_version: $latest_version"
+	echo "current_version: $current_version"
 }
 
 
 check(){
 
 	echo "Checking for version update..."
-	if [[ -f ~/horizon-xi/current_version ]]; then
-		current_version=$(cat ~/horizon-xi/current_version)
-		echo "Installed version: $current_version"
-		echo "Latest version: $latest_version"
-		if [[ ${current_version} == ${latest_version} ]]; then
-			echo "Latest already installed, nothing to do!"
-		else
-			echo "Updating and launching"
-			update
-			launch
-			echo "$latest_version" > ~/horizon-xi/current_version
-		fi
+	echo "Installed version: $current_version"
+	echo "Latest version: $latest_version"
+	if [[ ${current_version} == ${latest_version} ]]; then
+		echo "Latest already installed, nothing to do!"
+		launch
 	else
-		echo "No current_version exists, updating and logging"
 		echo "Updating and launching"
 		update
 		launch
-		echo "$latest_version" > ~/horizon-xi/current_version
 	fi
-
 }
 
 update(){
