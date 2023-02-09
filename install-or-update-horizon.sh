034e2bc8da6b4b2ef9ed2e654247e1fa20048333
Trent <tdyck@protonmail.com>
2023-02-08 20:26:48 -0800

    Add a non steam game, programatically

--- install-or-update-horizon.sh
+++ install-or-update-horizon.sh
@@ -51,6 +51,47 @@ check(){
 	fi
 }
 
+add_non_steam_game(){
+
+	app_name="Horizon XI"
+
+	# Source - https://github.com/sonic2kk/steamtinkerlaunch/issues/729
+	# 1 Download the latest release
+	if [[ $( which /home/deck/horizon-xi/stl/steamtinkerlaunch ) ]]; then
+	        echo "Steam tinker launch already installed, continuing..."
+	else
+	        stl_json=$(curl https://api.github.com/repos/sonic2kk/steamtinkerlaunch/releases)
+	        latest_stl_version=$(echo ${stl_json} | jq -r '.[].tag_name' | head -n1)
+	        stl_zip_url=$(echo ${stl_json} | jq -r '.[] | select(.tag_name=="'${latest_stl_version}'") | .zipball_url')
+	        echo "Downloading... ${stl_zip_url}"
+	fi
+
+	# 2 Add a non-steam game via stl
+	# docs - https://github.com/sonic2kk/steamtinkerlaunch/wiki/Add-Non-Steam-Game
+	./steamtinkerlaunch addnonsteamgame --appname="${app_name}" --exepath=/home/deck/horizon-xi/lib/net45/HorizonXI-Launcher.exe --startdir=/home/deck/horizon-xi/lib/net45/
+
+	# 3 Download pip
+	if [[ $(which /home/deck/.local/bin/pip) ]]; then 
+	        echo "pip already installed. Carrying on...";
+	else 
+	        wget https://bootstrap.pypa.io/get-pip.py
+	        python get-pip.py --user
+	fi
+	# 4 install vdf module
+	/home/deck/.local/bin/pip install vdf
+
+	# 6 load the vdf, grab the app_id
+	# Source - https://github.com/DavidoTek/ProtonUp-Qt/issues/175
+	shortcuts_vdf=$(find /home/deck/.local/share/Steam/ -name "shortcuts.vdf" -type f)
+	echo "Installing to $shortcuts_vdf"
+	app_id=$(python -c "import vdf; d=vdf.binary_loads(open('${shortcuts_vdf}', 'rb').read()); items = list(d['shortcuts'].values()); print([i for i in items if i['appname'] in ['${app_name}']][0]['appid']+2**32);")
+	echo "app_id: $app_id"
+	config_vdf=/home/deck/.local/share/Steam/config/config.vdf
+	python -c "import vdf; d=vdf.load(open('${config_vdf}')); ctm = d['InstallConfigStore']['Software']['Valve']['Steam']['CompatToolMapping']; ctm['${app_id}']={ 'name': 'GE-Proton7-42', 'config': '', 'priority': '250' }; vdf.dump(d, open('${config_vdf}','w'), pretty=True);"
+	echo "Successfully added nonsteam game"
+
+}
+
 update(){
 
 	echo "Creating required directories..."
@@ -81,12 +122,7 @@ update(){
 			# Install GE-Proton
 			echo "Launching ProtonQT..."
 			read -p "Install GE-Proton via ProtonQT, after adding proton, hit enter to continue" </dev/tty
-			# If someone can decipher this: https://developer.valvesoftware.com/wiki/Add_Non-Steam_Game
-			# maybe we can automate the game shortcut installation. until then, a manual effort:
-			steam steam://AddNonSteamGame
-			echo "Install by browsing to /home/deck/horixon-xi/lib/net45 & select file type All files. Choose HorizonXI-Launcher.exe"
-			echo "Right click the new entry, hit Properties > Compatibility"
-			echo "Change to the new version of Proton GE you just installed"
+			add_non_steam_game
 			# Low disk space version
 			if [[ "${sd_link}" == "true" ]]; then
 				read -p "Not enough disk space found on the main /home drive, install to SD Card? (Enter to continue, Ctrl + c to abort" </dev/tty
