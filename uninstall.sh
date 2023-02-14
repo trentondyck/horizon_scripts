66eaeab14c9f82b0ea5eee239e0d74640bea21cc
Trent <tdyck@protonmail.com>
2023-02-14 10:29:15 -0800

    Update uninstall for multiple horizoin instances (in case, derp)

--- uninstall.sh
+++ uninstall.sh
@@ -16,23 +16,26 @@ init(){
 			exit 2
 		fi
 	fi
-	export horizon_directory=$(echo $config_json | sed 's/pfx.*$//g')
-	export steam_id=$(grep -sir "Horizon XI" /home/deck/.local/share/Steam/userdata/ | grep screenshots | awk '{print $2}' | sed 's/"//g')
 }
 
 uninstall(){
 
-	rm -Rf ~/horizon-xi
-	if [[ $(ps -ef | grep steam | wc -l) -le 12 ]]; then
-		echo "Steam is not running. Start steam and try again"
-		exit 2
-	else
-        	steam steam://rungameid/${steam_id}
-	fi
-	app_id=$(ps -ef | grep horizon | grep AppId | awk '{print $10}' | sed 's/AppId=//g')
-	killall horizon
-	steam steam://uninstall/${app_id}
-	rm -Rf ${horizon_directory}
+	for cj in ${config_json}; do
+		export horizon_directory=$(echo $cj | sed 's/pfx.*$//g')
+		export steam_id=$(grep -sir "Horizon XI" /home/deck/.local/share/Steam/userdata/ | grep screenshots | awk '{print $2}' | sed 's/"//g')
+		rm -Rf ~/horizon-xi
+		if [[ $(ps -ef | grep steam | wc -l) -le 12 ]]; then
+			echo "Steam is not running. Start steam and try again"
+			exit 2
+		else
+        		steam steam://rungameid/${steam_id}
+		fi
+		# This may be dangerous, we may want to get this from python as with the installer script
+		app_id=$(ps -ef | grep horizon | grep AppId | awk '{print $10}' | sed 's/AppId=//g')
+		killall horizon
+		steam steam://uninstall/${app_id}
+		rm -Rf ${horizon_directory}
+	done
 
 }
 
