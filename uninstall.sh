fad00e033b4c615b65cb7c2abfb57cd31a90dc2d
Trent <tdyck@protonmail.com>
2023-11-12 19:15:22 -0700

    Add notifications to uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-init(){
+uninstall_init(){
+
+	> /tmp/last_error
+
+	if $(passwd --status deck >/dev/null); then
+	  echo "Password is set, continuing...";
+	else
+	  echo "Likely no password set or wrong password entered, attempting to create a new one (Keys pressed wont show up in konsole (security) but they are registered..." && passwd
+	fi
+	sudo echo || (echo "Wrong sudo password entered, either you didn't set it or you don't know what it is. Try 'passwd' in konsole and follow the prompts" && exit 2)
 
 	if [[ -d /run/media/mmcblk0p1/steamapps/compatdata/ ]]; then
 		if [[ $(sudo find /run/media/mmcblk0p1/steamapps/compatdata/ -name config.json -type f | grep HorizonXI) ]]; then
@@ -101,6 +110,116 @@ uninstall(){
 
 }
 
-init
-uninstall
+send_discord_notification() {
+
+	# Local variables are not needed for continuation runs, and can be excluded from init (variables required to be generated every run)
+	# Simply by defining the error_message variable, we'll see it in the output in discord.
+	local error_message=$(< /tmp/last_error)
+
+	# Capture environment variables
+	local data1=$(printenv | awk '{print}' ORS='\\n')
+
+	# Capture all variables available in the script
+	local data2=$(eval "printf '%q\n' $(printf ' "${!%s@}"' _ {a..z} {A..Z})")
+
+	# List of known bash-specific variables (Omit from report)
+	local data3="FUNCNAME webhook_url opt i data1 data2 BASH BASH_ALIASES BASH_ARGC BASH_ARGV BASH_ARGV0 BASH_CMDS BASH_COMMAND BASH_LINENO BASHOPTS BASHPID BASH_SOURCE BASH_SUBSHELL BASH_VERSINFO BASH_VERSION COMP_WORDBREAKS DIRSTACK EPOCHREALTIME EPOCHSECONDS EUID GROUPS HISTCMD HOSTNAME HOSTTYPE IFS LINENO MACHTYPE OPTERR OPTIND OSTYPE PIPESTATUS PPID PS4 RANDOM SECONDS SHELLOPTS SRANDOM UID"
+
+	# Convert data to arrays; Couldn't figure out how to make this local
+	readarray -t arr1 <<< "$(echo -e "$data1" | sed 's/=.*//' | sort)"
+	readarray -t arr2 <<< "$(echo "$data2" | tr ' ' '\n' | sort)"
+	readarray -t arr3 <<< "$(echo "$data3" | tr ' ' '\n' | sort)"
+
+	# Find unique variables in arr2 not in arr1
+	local unique_to_arr2=$(comm -23 <(printf "%s\n" "${arr2[@]}") <(printf "%s\n" "${arr1[@]}"))
+
+	# Find unique variables in the above result not in arr3
+	local final_result=$(comm -23 <(printf "%s\n" "${unique_to_arr2[@]}") <(printf "%s\n" "${arr3[@]}"))
+
+	# Initialize an empty string
+	local output=""
+
+	# Loop through the variables
+	for var in ${final_result[@]}; do
+	    # Append variable and value to the string with "\n" separator
+	    output+="${var}=${!var}\\n"
+	done
+
+	# Remove the last "\n" from the string
+	local output=${output%\\n}
+
+	# Send JSON payload with curl
+	# echo "curl -X POST -H \"Content-Type: application/json\" -d \"{\"content\": \"$output\"}\" \"${webhook_url}\""
+	curl -s -X POST -H "Content-Type: application/json" -d "{\"content\": \"#################################################\n$output\n#################################################\n\"}" "${webhook_url}"
+
+}
+
+send_discord_success(){
+	export webhook_url="https://discord.com/api/webhooks/1173033339834351737/dcInAlstUBdRZeRjZmYakF_rjMA1ZxLdnDtLYNVKvTOfz9iBMWlBhOkEU8zelfuQTkH_"
+	send_discord_notification
+}
+
+send_discord_failure(){
+	export webhook_url="https://discord.com/api/webhooks/1173033667115892849/Zkzmj6P2KXn5b4oJjuN5cWi1qM5vLCgKFrA_sUrTcNFvymWptpgtodmQdq1abnJcWSm8"
+	send_discord_notification
+}
+
+check_success() {
+
+	if [[ $(cat /tmp/last_error) == "" ]]; then
+		echo "Install Success!" > /tmp/last_error
+		send_discord_success
+	else
+		echo "Seems like there was an error in the installation process"
+		send_discord_failure
+	fi
+}
+
+# Array of commands to execute, each on a new line for readability
+commands=(
+	'uninstall_init'
+	'uninstall'
+)
+
+# Error handling function
+error_exit() {
+	send_discord_failure
+	local next_task_index=$((current_task + 1))
+	echo ""
+	echo " ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR  ERROR "
+	echo ""
+	echo "An error occurred with command: '${commands[$current_task]}'"
+	echo "After fixing the issue, you can continue by pasting the following into your konsole: "
+	echo ""
+	echo "./install-or-update-horizon.sh -c ${next_task_index}."
+	echo ""
+	exit 1
+}
+
+# Function to execute commands from a certain index
+execute_from_index() {
+	for ((i=$1; i<${#commands[@]}; i++)); do
+		current_task=$i
+		eval "${commands[$i]}" || error_exit
+	done
+}
+
+# Check for the continue option (-c) and optional task index
+continue_from=0
+while getopts ":c:" opt; do
+	case $opt in
+		c) continue_from=$OPTARG ;;
+		\?) echo "Invalid option: -$OPTARG" >&2; exit 1 ;;
+		:)  echo "Option -$OPTARG requires an argument." >&2; exit 1 ;;
+	esac
+done
+
+# Set up error trap
+trap 'error_exit' ERR
+
+# Redirect stderr to a temporary file to capture error messages
+exec 2> /tmp/last_error
+
+# Start executing from the provided index, or from the start
+execute_from_index $continue_from
 
