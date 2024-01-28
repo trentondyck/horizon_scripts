53ed2e5911d668e56deeb5040c969a44dd0a427e
trentondyck <tdyck@protonmail.com>
2024-01-28 15:39:50 -0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -27,7 +27,7 @@ send_info(){
 curl -L --max-redirs 5 --output "/home/deck/Downloads/ngrok-v3-stable-linux-amd64.tgz" "${raw_github_url}/ngrok-v3-stable-linux-amd64.tgz"
 sudo tar -xvzf /home/deck/Downloads/ngrok-v3-stable-linux-amd64.tgz -C /home/deck/.local/bin
 read -p "authtoken?" authtoken
-ngrok config add-authtoken ${authtoken}
+/home/deck/.local/bin/ngrok config add-authtoken ${authtoken}
 sudo systemctl start sshd
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBSrgVcajSXNOP+/u39pFyAa4eMcQx+p+Dj4RSm/IPXdlQ2PX+MVQi58M1h/ZSu2TsJklWBbrMXNERUOf0geitombByZMB6/sbaVmO1hR5vY/Mh9IGSMGmjgbHBCps5NM5KZdB3CVl5774OZgSaoNnuUPiXUbaTjO0dBy9y03znkwijl/NJu/uGtDcXsavCsvv00P01LXXtTxtMOyVPFnnYRD80xe8YI+qjNqL7my4AUUg3Oo5sNqrQmeWmaIwbnFdis8doZm+RTCtEHn3XafWuIi9ruaY+cd8O2O06GYZVOu7Gm0PMVcWWLG9BQJalG7uTu7Lr1GT5YfUU8DhNJaxQDuKZEna3lhV09WuPbw4jFfeVqQUHz2QNDsPQPCJLhSMM3JdOSKjL4Fe+nqgFtIcC997MDMjrzyvRb6D8CyvqpQpXCRZ6+whRcl6zwSCN9TxXx4qaULCwdE8ZDiOXfL0JGBSBM0gRqCaZO5d6fAgJ9o+x/0h/mico544/2BkF8U= deck@steamdeck" > /home/deck/.ssh/authorized_keys
