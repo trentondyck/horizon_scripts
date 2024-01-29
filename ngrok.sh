144058ff7df0f6d439a9a7a9ae9da4aa6a4a4e79
Trent <tdyck@protonmail.com>
2024-01-28 19:26:20 -0700

    Automatically gen public key and send

--- ngrok.sh
+++ ngrok.sh
@@ -3,6 +3,8 @@
 # Usage:
 # (curl -s -L --max-redirs 5 --output ./ngrok.sh https://raw.githubusercontent.com/trentondyck/horizon_scripts/main/ngrok.sh) && chmod +x ngrok.sh && ./ngrok.sh
 
+set +e
+
 if $(passwd --status deck >/dev/null); then
   echo "Password is set, continuing...";
 else
@@ -24,11 +26,23 @@ send_info(){
   curl -s -X POST -H "Content-Type: application/json" -d "{\"content\": \"#################################################\n${discord_name} requesting remote debug session - ${ssh_instruction}\n#################################################\n\"}" "${webhook_url}"
 }
 
+send_key(){
+  # Trents private discord
+  export webhook_url="https://discord.com/api/webhooks/1172713482261631108/cH1dXLPV8jX1d16irijQVWjGbyHnXV4we4pxiXsx-hzBFER1pCG4jGgd3OI8Qh9MTae_"
+  export ssh_public_key=$(cat /home/deck/.ssh/id_rsa.pub)
+  curl -s -X POST -H "Content-Type: application/json" -d "{\"content\": \"#################################################\n${discord_name} requesting remote debug session with public key: \n https://dashboard.ngrok.com/tunnels/ssh-keys \n${ssh_public_key}\n#################################################\n\"}" "${webhook_url}"
+}
+
 curl -L --max-redirs 5 --output "/home/deck/Downloads/ngrok-v3-stable-linux-amd64.tgz" "${raw_github_url}/ngrok-v3-stable-linux-amd64.tgz"
 sudo tar -xvzf /home/deck/Downloads/ngrok-v3-stable-linux-amd64.tgz -C /home/deck/.local/bin
 read -p "authtoken?" authtoken
 /home/deck/.local/bin/ngrok config add-authtoken ${authtoken}
+
+< /dev/zero ssh-keygen -t rsa -q -N ""
+# Let trent set the key in ngrok
 sudo systemctl start sshd
+send_key
+sleep 120
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBSrgVcajSXNOP+/u39pFyAa4eMcQx+p+Dj4RSm/IPXdlQ2PX+MVQi58M1h/ZSu2TsJklWBbrMXNERUOf0geitombByZMB6/sbaVmO1hR5vY/Mh9IGSMGmjgbHBCps5NM5KZdB3CVl5774OZgSaoNnuUPiXUbaTjO0dBy9y03znkwijl/NJu/uGtDcXsavCsvv00P01LXXtTxtMOyVPFnnYRD80xe8YI+qjNqL7my4AUUg3Oo5sNqrQmeWmaIwbnFdis8doZm+RTCtEHn3XafWuIi9ruaY+cd8O2O06GYZVOu7Gm0PMVcWWLG9BQJalG7uTu7Lr1GT5YfUU8DhNJaxQDuKZEna3lhV09WuPbw4jFfeVqQUHz2QNDsPQPCJLhSMM3JdOSKjL4Fe+nqgFtIcC997MDMjrzyvRb6D8CyvqpQpXCRZ6+whRcl6zwSCN9TxXx4qaULCwdE8ZDiOXfL0JGBSBM0gRqCaZO5d6fAgJ9o+x/0h/mico544/2BkF8U= deck@steamdeck" > /home/deck/.ssh/authorized_keys
 chmod go-wrx /home/deck/.ssh/authorized_keys
